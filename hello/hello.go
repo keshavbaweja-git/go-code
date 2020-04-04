274da25d7e5a592f6fa3a91bf5e85042de4c4475
Keshav Baweja <keshavbaweja@gmail.com>
2020-04-04 18:59:07 +0800

    Methods

--- hello/hello.go
+++ hello/hello.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/keshavbaweja-git/hello/morestrings"
+	"github.com/keshavbaweja-git/hello/rect"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("Hello, world."))
+	rect1 := rect.Rect{L: 10, B: 5}
+	fmt.Println(rect1.Area())
 }
